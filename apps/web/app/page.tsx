6f72e459eb6e9c7fc4529e31d6b83676a6eb93a8
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-08-06 01:11:03 +0530

    feat: convex to widget

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
-"use client"
-import { api } from "@workspace/backend/_generated/api"
-import { useQuery } from "convex/react"
+"use client";
+import { api } from "@workspace/backend/_generated/api";
+import { useQuery } from "convex/react";
 export default function Page() {
-  const users = useQuery(api.users.getmany)
+  const users = useQuery(api.users.getmany);
   return (
     <div className="flex items-center justify-center h-screen">
       <p> apps/web </p>
