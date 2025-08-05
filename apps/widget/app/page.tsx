c5a59045bf8af438744d5a052663cecf44fc042b
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-08-05 23:19:08 +0530

    feat(package): internal package example

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/internal-package/add";
+
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World apps/web</h1>
+        <h1 className="text-2xl font-bold">Hello World app/widget</h1>
         <Button size="sm">Button</Button>
       </div>
-      <div>adding 2 and 3 : {add(2, 3)}</div>
     </div>
   );
 }
